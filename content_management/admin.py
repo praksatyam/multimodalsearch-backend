06683285327405a157130196ae09a54caa7e1c25
Satyam <satyam.tud@gmail.com>
2025-09-08 23:26:31 +0200

    aws-s3

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Register ContentItem subclasses for admin interface
+from .models import TextDocument, Video, Image, LectureNote, UploadedFile
+
+admin.site.register(TextDocument)
+admin.site.register(Video)
+admin.site.register(Image)
+admin.site.register(LectureNote)
+admin.site.register(UploadedFile)
